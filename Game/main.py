ec4f552c27ad40896b7d06b1db2a1ddd8ae9e406
jake-yummyburger <jake.sherlock506@education.nsw.gov.au>
2026-03-26 15:20:09 +1100

    fixing up win conditions

--- Game/main.py
+++ Game/main.py
@@ -6,23 +6,35 @@ def play():
 
     if user_choice not in choices:
         print("invalid choice. You know how rock, paper, scissors works, right?")
-        return 
+        return False
 
     print(f"you chose: {user_choice}")
     computer_choice = random.choice(choices)
     print(f"computer_chose: {computer_choice}")
 
+    you_win = False
     if user_choice == computer_choice:
         print("Damn, It's a tie.")
 
-        if (user_choice == rock and computer_choice == scissors):
-         print("diggity, darn, damn, dang.. You win. how about one more?")
-        if (user_choice == paper and computer_choice == rock):
-         print("Gr.. You're just getting lucky. Let's go again!")
-        if (user_choice == scissors and computer_choice == paper):
-         print("Awh, I lost. Let's go again, I'll get You this time!")
+    if (user_choice == "rock" and computer_choice == "scissors"):
+        print("diggity, darn, damn, dang.. You win. how about one more?")
+        you_win = True
+    if (user_choice == "paper" and computer_choice == "rock"):
+        print("Gr.. You're just getting lucky. Let's go again!")
+        you_win = True
+    if (user_choice == "scissors" and computer_choice == "paper"):
+        print("Awh, I lost. Let's go again, I'll get You this time!")
+        you_win = True
+    if (user_choice == "" and computer_choice == ""):
+        print("Screw you bro >:( I'll get you this time")
+        you_win = True
 
+    if(not you_win):
+       print("haha, you lose!")
 
+    return False
 
-if __name__ == "__main__":
-    play()
+print("Welcome, to Rock, Paper, Scissors or exit")
+finished = False
+while not finished:
+    finished = play()
